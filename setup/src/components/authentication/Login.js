ffe819fbe36bde50b9a9bdc6fdcb32bfa60b8921
Atharva Mohite <atharvamohite20@gmail.com>
2021-07-09 01:37:13 +0530

    Login and Signup template added

--- setup/src/components/authentication/Login.js
+++ setup/src/components/authentication/Login.js
@@ -27,43 +27,110 @@ const Login = () => {
   }
 
   return (
-    <div className="auth-wrapper">
-      <div className="container">
-        <h1 className="signup__header">Login</h1>
-        {error && <p>{error}</p>}
-        <form action="" onSubmit={handleSubmit}>
-          <div className="form__control">
-            <div className="form__label">Email</div>
-            <input
-              className="form__input"
-              type="email"
-              ref={emailRef}
-              required
-            ></input>
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8 bg-white p-8 rounded shadow-md">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-center text-4xl font-extrabold text-gray-900">
+              Log in to your account
+            </h2>
           </div>
-          <div className="form__control">
-            <div className="form__label">Password</div>
-            <input
-              className="form__input"
-              type="password"
-              ref={passwordRef}
-              required
-            ></input>
+          {error && <p>{error}</p>}
+          <form
+            className="mt-8 space-y-6"
+            method="POST"
+            onSubmit={handleSubmit}
+          >
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  ref={emailRef}
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  ref={passwordRef}
+                  autoComplete="current-password"
+                  required
+                  className="helper appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <Link
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                  to="/forgot-password"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                disabled={loading}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-lg font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+          <div className="flex justify-between items-center mt-3">
+            <hr className="w-full" />
+            <span class="text-gray-400 mx-2 flex">Or</span>
+            <hr className="w-full" />
           </div>
 
-          <button disabled={loading} className="btn" type="submit">
-            Log In
-          </button>
-        </form>
-        <small>
-          Forgot
-          <Link to="/forgot-password"> Password?</Link>
-        </small>
-        <div className="footer-text">
-          Need an account? <Link to="/signup">Sign Up</Link>
+          <p className="text-center text-sm">
+            Need an account?{" "}
+            <span className="ml-1 text-indigo-600 hover:text-indigo-500 cursor-pointer border-indigo-300 border-b-2 font-bold">
+              <Link to="/signup">Sign Up</Link>
+            </span>{" "}
+          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- setup/src/components/authentication/Signup.js
+++ setup/src/components/authentication/Signup.js
@@ -45,50 +45,96 @@ const Signup = () => {
   }
 
   return (
-    <div className="auth-wrapper">
-      <div className="container">
-        <h1 className="signup__header">Signup</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8 bg-white p-8 rounded shadow-md">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-4xl font-extrabold text-gray-900">
+            Sign up into a new account
+          </h2>
+        </div>
         {error && <p>{error}</p>}
-        <form action="" onSubmit={handleSubmit}>
-          <div className="form__control">
-            <div className="form__label">Email</div>
-            <input
-              className="form__input"
-              type="email"
-              ref={emailRef}
-              required
-            ></input>
+        <form className="mt-8 space-y-6" method="POST" onSubmit={handleSubmit}>
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                ref={emailRef}
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                ref={passwordRef}
+                autoComplete="current-password"
+                required
+                className="helper appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password Confirmation
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                ref={passwordConfirmRef}
+                autoComplete="current-password"
+                required
+                className="helper appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password Confirmation"
+              />
+            </div>
           </div>
-          <div className="form__control">
-            <div className="form__label">Password</div>
-            <input
-              className="form__input"
-              type="password"
-              ref={passwordRef}
-              required
-            ></input>
+
+          <div>
+            <button
+              type="submit"
+              disabled={loading}
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-lg font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
           </div>
-          <div className="form__control">
-            <div className="form__label">Password Confirmation</div>
-            <input
-              className="form__input"
-              type="password"
-              ref={passwordConfirmRef}
-              required
-            ></input>
-          </div>
-          <button disabled={loading} className="btn" type="submit">
-            Sign Up
-          </button>
         </form>
+        <div className="flex justify-between items-center mt-3">
+          <hr className="w-full" />
+          <span class="text-gray-400 mx-2 flex">Or</span>
+          <hr className="w-full" />
+        </div>
 
         <StyledFirebaseAuth
           uiConfig={uiConfig}
           firebaseAuth={firebase.auth()}
         />
-        <div className="footer-text">
-          Already have an account? <Link to="/login">Log In</Link>
-        </div>
+
+        <p className="text-center text-sm">
+          Already have an account?{" "}
+          <span className="ml-1 text-indigo-600 hover:text-indigo-500 cursor-pointer border-indigo-300 border-b-2 font-bold">
+            <Link to="/login">Log In</Link>
+          </span>{" "}
+        </p>
       </div>
     </div>
   );
