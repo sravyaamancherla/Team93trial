61da93c99ee6ad774fc11df0f66ef56d47a710cd
Atharva Mohite <atharvamohite20@gmail.com>
2021-07-09 13:18:21 +0530

    Landing Page Added

--- setup/src/components/authentication/Dashboard.js
+++ setup/src/components/authentication/Dashboard.js
@@ -6,14 +6,20 @@ const Dashboard = () => {
 
   return (
     <>
-      {/* <div>
-        <h1>From Dashboard</h1>
-        <p>{currentUser && currentUser.email}</p>
-      </div> */}
-      {/* {console.log(currentUser)} */}
-      {/* {error && <p>{error}</p>} */}
-
-      {/* <Link to="/main/update-profile">Update Profile</Link> */}
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          {/* Replace with your content */}
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+          </div>
+          {/* /End replace */}
+        </div>
+      </main>
     </>
   );
 };
--- setup/src/components/landing-page/LandingPage.js
+++ setup/src/components/landing-page/LandingPage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Features from "./Features";
+import Hero from "./Hero";
+import Cta from "./Cta";
+import Contact from "./Contact";
+import Footer from "./Footer";
 
 const LandingPage = () => {
   return (
     <div>
-      <h1>Landing Page</h1>
+      <Hero />
+      <Features />
+      <Cta />
+      <Contact />
+      <Footer />
     </div>
   );
 };
